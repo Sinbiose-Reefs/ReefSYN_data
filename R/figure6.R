2f048b349154c2d1a5e46985bcf5c804140a3767
andreluza <luza.andre@gmail.com>
2023-01-24 14:31:42 -0300

    code fig 6

--- R/figure6.R
+++ R/figure6.R
@@ -1,7 +1,8 @@
 library(tidyverse)
+library (here)
 
 # ler arquivos
-caminho <- "~/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format" 
+caminho <- here ("DwC_output") 
 files <- list.files(pattern = 'DF_occ.csv', recursive = T, path = caminho, full.names = TRUE) # -> includes all subfolders
 
 # myfiles = lapply(files, read.csv)
@@ -9,43 +10,79 @@ files <- list.files(pattern = 'DF_occ.csv', recursive = T, path = caminho, full.
 tbl1 <- list.files(pattern = 'DF_occ.csv', recursive = T, path = caminho, full.names = TRUE) %>% 
   map_df(~ read_csv(., col_types = cols(.default = "c"), id = "name"))
 
-peld_bentos <- read.csv("~/git/ReefSYN_data/Data/PELD_iloc_benthos/DF_occ2.txt") %>% 
+peld_bentos <- read.csv( here ("DwC_output", "PELD_iloc_benthos", "DF_occ.txt")) %>% 
   mutate(name = "PELD_iloc_benthos")
-peld_peixe <- read.csv("~/git/ReefSYN_data/Data/PELD_iloc_fish/DF_occ.txt") %>% 
+peld_peixe <- read.csv(here ("DwC_output", "PELD_iloc_fish" , "DF_occ.txt"),sep=";") %>% 
   mutate(name = "PELD_iloc_fish")
-RN_benthos <- read.csv("~/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/GLongo_NRoss_spatialData/DF_occ_benthos.csv") %>% 
+RN_benthos <- read.csv(here ("DwC_output", "GLongo_NRoss_spatialData", "DF_occ_benthos.csv")) %>% 
   mutate(name = "RN_benthos")
-RN_fish <- read.csv("~/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/GLongo_NRoss_spatialData/DF_occ_fish.csv") %>% 
+RN_fish <- read.csv(here ("DwC_output","GLongo_NRoss_spatialData", "DF_occ_fish.csv")) %>% 
   mutate(name = "RN_fish")
-abrolhos_fish <- read.csv("~/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/RFrancini_timeSeries_abrolhos/DF_occ_fish.csv") %>% 
+abrolhos_fish <- read.csv(here ("DwC_output", "RFrancini_timeSeries_abrolhos", "DF_occ_fish.csv")) %>% 
   mutate(name = "abrolhos_fish")
-abrolhos_bentos <- read.csv("~/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/RFrancini_timeSeries_abrolhos/DF_occ_benthos.csv") %>% 
+abrolhos_bentos <- read.csv(here ("DwC_output", "RFrancini_timeSeries_abrolhos", "DF_occ_benthos.csv")) %>% 
   mutate(name = "abrolhos_bentos")
+alcatrazes_fish <- read.csv(here ("DwC_output", "Alcatrazes_time_series", "DF_occ.csv")) %>% 
+  mutate(name = "Alcatrazes_time_series")
 
 
-nomes_datasets <- data.frame(nome = c("III (X)", "III", "I (VIII)", 
-                                         "II", "II (IX)", "VI", 
-                                         "I", "V", "IV (XI)", "IV",
-                                         "VII", "V (XII)"),
-                             dataset = c("PELD_iloc_benthos", "PELD_iloc_fish", "AAued_spatialData",
-                                      "GLongo_spatialData", "RFrancini_spatialData", "RJ_time_series",
-                                      "RMorais_spatialData", "SC_time_series", "RN_benthos", "RN_fish",
-                                      "abrolhos_fish", "abrolhos_bentos")) %>% 
-  mutate(nome = factor(nome, levels = c("I", "II", "III", "IV", "V", "VI", "VII",
-                                              "I (VIII)", "II (IX)", "III (X)", "IV (XI)", "V (XII)")))
+nomes_datasets <- data.frame(nome = c("I", 
+                                      "II", 
+                                      "III", 
+                                      "IV", 
+                                      "V",
+                                      "VI", 
+                                      "VII", 
+                                      "VIII", 
+                                      "IX", 
+                                      "X",
+                                      "XI", 
+                                      "XII",
+                                      "XIII"),
+                             
+                             dataset = c(
+                               # fish
+                               "RMorais_spatialData",
+                               "abrolhos_fish", 
+                               "RJ_time_series",
+                               "PELD_iloc_fish", 
+                               "SC_time_series",
+                               "GLongo_spatialData", 
+                               "Alcatrazes_time_series",
+                               "RN_fish",
+                               # benthos
+                               "abrolhos_bentos",
+                               "RFrancini_spatialData", 
+                               "AAued_spatialData",
+                               "PELD_iloc_benthos", 
+                               "RN_benthos")) %>% 
+  mutate(nome = factor(nome, levels = c("I", 
+                                        "II", 
+                                        "III", 
+                                        "IV",
+                                        "V",
+                                        "VI",
+                                        "VII",
+                                        "VIII", 
+                                        "IX", 
+                                        "X", 
+                                        "XI", 
+                                        "XII", 
+                                        "XIII")))
   
   
 ## Fish
 
-tbl <- bind_rows(peld_bentos %>% select(scientificName, name),
-                 peld_peixe %>% select(scientificName, name),
-                 tbl1 %>% select(scientificName, name),
-                 RN_benthos %>% select(scientificName, name),
-                 RN_fish %>% select(scientificName, name),
-                 abrolhos_fish %>% select(scientificName, name),
-                 abrolhos_bentos %>% select(scientificName, name)) %>% 
+tbl <- bind_rows(peld_bentos %>% select(scientificNameAccepted, name),
+                 peld_peixe %>% select(scientificNameAccepted, name),
+                 tbl1 %>% select(scientificNameAccepted, name),
+                 RN_benthos %>% select(scientificNameAccepted, name),
+                 RN_fish %>% select(scientificNameAccepted, name),
+                 abrolhos_fish %>% select(scientificNameAccepted, name),
+                 abrolhos_bentos %>% select(scientificNameAccepted, name),
+                 alcatrazes_fish %>% select(scientificNameAccepted, name)) %>% 
   distinct() %>% 
-  mutate(name = gsub("/Users/cesarcordeiro/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/", "", name)) %>% 
+  mutate(name = gsub("D:/Pos_Doc_Sinbiose/ReefSYN_data/DwC_output/", "", name)) %>% 
   mutate(name = gsub("/DF_occ.csv", "", name))
 
 # tbl %>% 
@@ -60,29 +97,29 @@ tbl <- bind_rows(peld_bentos %>% select(scientificName, name),
 # Get full taxonomical classification
 fish <- tbl %>% 
   filter(!name %in% c("PELD_iloc_benthos", "AAued_spatialData", "RN_benthos", "RFrancini_spatialData", "abrolhos_bentos")) %>%
-  distinct(scientificName) %>% 
-  arrange(scientificName) %>% 
-  filter(scientificName != "Menaphorus punticulatus") %>%
+  distinct(scientificNameAccepted) %>% 
+  arrange(scientificNameAccepted) %>% 
+  filter(scientificNameAccepted != "Menaphorus punticulatus") %>%
   pull() %>% 
-  worms::wormsbymatchnames()
+  worms::wormsbymatchnames(verbose=F)
 
 # Table with species and datasets
-fish %>% 
-  select(kingdom:genus, valid_name, scientificname) %>% 
-  arrange(class, order, family, genus, valid_name) %>% 
-  mutate(tirar = ifelse(genus == valid_name, "tirar", NA)) %>% 
-  filter(is.na(tirar)) %>%
-  left_join(., tbl %>% 
-              select(scientificName, name) %>% 
-              distinct() %>% 
-              mutate(name = gsub("/Users/cesarcordeiro/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/", "", name)) %>% 
-              mutate(name = gsub("/DF_occ.csv", "", name)) %>% 
-              filter(!is.na(scientificName)) %>% 
-              rename(scientificname = scientificName,
-                     dataset = name)) %>% 
-  select(-tirar) %>% 
-  distinct(order) %>% 
-  pull() %>% noquote()
+#fish %>% 
+#  select(kingdom:genus, valid_name, scientificname) %>% 
+#  arrange(class, order, family, genus, valid_name) %>% 
+#  mutate(tirar = ifelse(genus == valid_name, "tirar", NA)) %>% 
+#  filter(is.na(tirar)) %>%
+#  left_join(., tbl %>% 
+#              select(scientificNameAccepted, name) %>% 
+#              distinct() %>% 
+#              mutate(name = gsub("D:/Pos_Doc_Sinbiose/ReefSYN_data/DwC_output/", "", name)) %>% 
+#              mutate(name = gsub("/DF_occ.csv", "", name)) %>% 
+#              filter(!is.na(scientificNameAccepted)) %>% 
+#              rename(scientificname = scientificNameAccepted,
+#                     dataset = name)) %>% 
+#  select(-tirar) %>% 
+#  distinct(order) %>% 
+#  pull() %>% noquote()
   # pivot_wider(names_from = dataset, values_from = dataset) %>% 
   # knitr::kable()
 
@@ -102,12 +139,12 @@ a <- fish %>%
   mutate(tirar = ifelse(genus == valid_name, "tirar", NA)) %>% 
   filter(is.na(tirar)) %>%
   left_join(., tbl %>% 
-              select(scientificName, name) %>% 
+              select(scientificNameAccepted, name) %>% 
               distinct() %>% 
-              mutate(name = gsub("/Users/cesarcordeiro/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/", "", name)) %>% 
+              mutate(name = gsub("D:/Pos_Doc_Sinbiose/ReefSYN_data/DwC_output/", "", name)) %>% 
               mutate(name = gsub("/DF_occ.csv", "", name)) %>% 
-              filter(!is.na(scientificName)) %>% 
-              rename(scientificname = scientificName,
+              filter(!is.na(scientificNameAccepted)) %>% 
+              rename(scientificname = scientificNameAccepted,
                      dataset = name)) %>% 
   select(-tirar) %>% 
   group_by(dataset) %>% 
@@ -127,20 +164,24 @@ a <- fish %>%
         axis.title.y = element_text(size = 14),
         axis.text.y = element_text(size = 12),
         axis.text.x = element_text(size = 12)) +
-  labs(y = "taxa richness (n)", x = "", title = "(a) fish taxa") +
+  labs(y = "Taxa richness (n)", x = "", title = "(a) Fish taxa") +
   scale_fill_brewer(palette="Spectral")
   
   
   
 ## Benthos
 bentos <- tbl %>% 
-  filter(name %in% c("PELD_iloc_benthos", "AAued_spatialData", "RN_benthos", "RFrancini_spatialData", "abrolhos_bentos")) %>%
-  distinct(scientificName) %>% 
-  arrange(scientificName) %>% 
-  filter(!scientificName %in% c(NA, "Turf", "Sand gravel", "Others", "Corticated",
+  filter(name %in% c("PELD_iloc_benthos", 
+                     "AAued_spatialData", 
+                     "RN_benthos", 
+                     "RFrancini_spatialData", 
+                     "abrolhos_bentos")) %>%
+  distinct(scientificNameAccepted) %>% 
+  arrange(scientificNameAccepted) %>% 
+  filter(!scientificNameAccepted %in% c(NA, "Turf", "Sand gravel", "Others", "Corticated",
                                 "Foliose", "Filamentous", "Leathery", "Unknown")) %>% 
   pull() %>% 
-  worms::wormsbymatchnames()
+  worms::wormsbymatchnames(verbose=F)
 
 # bentos %>% 
 #   select(kingdom:genus, valid_name) %>% 
@@ -163,12 +204,12 @@ b <- bentos %>%
   arrange(kingdom, phylum, class, order, family, genus, valid_name) %>% 
   mutate(tirar = ifelse(genus == valid_name, "tirar", NA)) %>% 
   left_join(., tbl %>% 
-              select(scientificName, name) %>% 
+              select(scientificNameAccepted, name) %>% 
               distinct() %>% 
               mutate(name = gsub("/Users/cesarcordeiro/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/", "", name)) %>% 
               mutate(name = gsub("/DF_occ.csv", "", name)) %>% 
-              filter(!is.na(scientificName)) %>% 
-              rename(scientificname = scientificName,
+              filter(!is.na(scientificNameAccepted)) %>% 
+              rename(scientificname = scientificNameAccepted,
                      dataset = name)) %>%
   group_by(dataset) %>% 
   summarise(Species = n_distinct(valid_name),
@@ -187,7 +228,7 @@ b <- bentos %>%
           axis.title.y = element_text(size = 14),
           axis.text.y = element_text(size = 12),
           axis.text.x = element_text(size = 12)) +
-    labs(y = "", x = "", title = "(b) benthic taxa") +
+    labs(y = "", x = "", title = "(b) Benthic taxa") +
   scale_fill_brewer(palette="Spectral")
 
 # legenda
@@ -198,6 +239,7 @@ get_legend <- function(myggplot){
   legend <- tmp$grobs[[leg]]
   return(legend)
 }
+
 #### --- ###
 
 c <- bentos %>% 
@@ -205,12 +247,12 @@ c <- bentos %>%
   arrange(kingdom, phylum, class, order, family, genus, valid_name) %>% 
   mutate(tirar = ifelse(genus == valid_name, "tirar", NA)) %>% 
   left_join(., tbl %>% 
-              select(scientificName, name) %>% 
+              select(scientificNameAccepted, name) %>% 
               distinct() %>% 
-              mutate(name = gsub("/Users/cesarcordeiro/Meu Drive/PARCERIAS/SiNBIOSE/dados/Darwin Core Format/", "", name)) %>% 
+              mutate(name = gsub("D:/Pos_Doc_Sinbiose/ReefSYN_data/DwC_output/", "", name)) %>% 
               mutate(name = gsub("/DF_occ.csv", "", name)) %>% 
-              filter(!is.na(scientificName)) %>% 
-              rename(scientificname = scientificName,
+              filter(!is.na(scientificNameAccepted)) %>% 
+              rename(scientificname = scientificNameAccepted,
                      dataset = name)) %>%
   group_by(dataset) %>% 
   summarise(Species = n_distinct(valid_name),
