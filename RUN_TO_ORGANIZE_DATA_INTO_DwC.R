ec2a8f9f8c4940568bb3525f11cd4c9e1212f72f
andreluza <luza.andre@gmail.com>
2023-08-16 14:23:50 -0300

    edit code to generate DwC formatted datasets

--- RUN_TO_ORGANIZE_DATA_INTO_DwC.R
+++ RUN_TO_ORGANIZE_DATA_INTO_DwC.R
@@ -8,85 +8,45 @@ require(here)
 # create a folder to host datasets organized according to darwin core standards
 
 dir.create ("DwC_output")
-dir.create (here ("DwC_output", "AAued_spatialData"))
-dir.create (here ("DwC_output", "Alcatrazes_time_series"))
-dir.create (here ("DwC_output", "GLongo_NRoss_spatialData"))
-dir.create (here ("DwC_output", "GLongo_spatialData"))
-dir.create (here ("DwC_output", "RFrancini_spatialData"))
-dir.create (here ("DwC_output", "RFrancini_timeSeries_abrolhos"))
-dir.create (here ("DwC_output", "RJ_time_series"))
-dir.create (here ("DwC_output", "RMorais_spatialData"))
-dir.create (here ("DwC_output", "SC_time_series"))
-dir.create (here ("DwC_output", "PELD_iloc_benthos"))
-dir.create (here ("DwC_output", "PELD_iloc_fish"))
+# fish datasets
+dir.create (here ("DwC_output", "RMorais_spatialData")) # dataset I
+dir.create (here ("DwC_output", "RFrancini_timeSeries_abrolhos")) # datasets II and XII
+dir.create (here ("DwC_output", "RJ_time_series")) # dataset III
+dir.create (here ("DwC_output", "PELD_iloc_fish")) # dataset IV
+dir.create (here ("DwC_output", "Pinheiro_TrindadeMVaz")) # dataset V
+dir.create (here ("DwC_output", "SC_time_series")) # dataset VI
+dir.create (here ("DwC_output", "Pinheiro_GuarapariES")) # dataset VII 
+dir.create (here ("DwC_output", "Pinheiro_ES")) # dataset VIII
+dir.create (here ("DwC_output", "GLongo_spatialData")) # dataset IX
+dir.create (here ("DwC_output", "Alcatrazes_time_series"))  # dataset X
+dir.create (here ("DwC_output", "GLongo_NRoss_spatialData")) # dataset XI and XVI
 
+# benthic datasets
+dir.create (here ("DwC_output", "RFrancini_spatialData")) # dataset XIII
+dir.create (here ("DwC_output", "AAued_spatialData")) # dataset XIV
+dir.create (here ("DwC_output", "PELD_iloc_benthos")) # dataset XV
 
 
 # ---------------------
 
-# organize data of Aued et al. 2018
+# fish datasets
+source (here ("Data","occ_Morais_et_al", "data_organization.R")) # organize dataset I
+source (here ("Data","occ_Francini_temporal_abrolhos", "data_organization_fish.R")) # organize dataset II
+source (here ("Data","occ_Mendes_temporal_arraial", "data_organization.R")) # organize dataset III
+source (here ("Data","PELD_iloc_fish", "data_organization.R")) # organize dataset IV
+source (here ("Data","occ_Pinheiro_Trindade", "data_organization.R")) # organize dataset V
+source (here ("Data","occ_Floeter_temporal_santa_catarina", "data_organization.R")) # organize dataset VI
+source (here ("Data","occ_Pinheiro_Guarapari", "data_organization.R")) # organize dataset VII
+source (here ("Data","occ_Pinheiro_ES", "data_organization.R")) # organize dataset VIII
+source (here ("Data","occ_Longo_et_al", "data_organization.R")) # organize dataset IX
+source (here ("Data","occ_Quimbayo_temporal_alcatrazes", "data_organization.R")) # organize dataset X
+source (here ("Data","occ_RN_Norte_Longo", "data_organization.R")) # organize dataset XI and XVI
 
-source (here ("Data","occ_Aued_et_al", "org_bentos_data.R"))
+# benthic datasets
+source (here ("Data","occ_Francini_temporal_abrolhos", "data_organization_benthos.R")) # organize dataset XII
+source (here ("Data","occ_Francini_et_al", "data_organization.R")) # organize dataset XIII
+source (here ("Data","occ_Aued_et_al", "data_organization.R")) # organize dataset XIV
+source (here ("Data","PELD_iloc_benthos", "data_organization.R")) # organize dataset XV
 
-
-
-# organize data of Alcatrazes
-
-source (here ("Data","occ_Quimbayo_temporal_alcatrazes", "data_organization.R"))
-
-
-# organize data of Quimbayo et al. 2023 Santa Catarina
-
-source (here ("Data","occ_Floeter_temporal_santa_catarina", "data_organization.R"))
-
-
-
-
-# organize data of Francini & Santana et al. unpublished
-
-source (here ("Data","occ_Francini_et_al", "Script_data_organization_francini.R"))
-
-
-
-
-# organize data of Francini et al Time Series of Abrolhos
-# fish
-source (here ("Data","occ_Francini_temporal_abrolhos", "data_organization_fish.R"))
-
-# benthos
-source (here ("Data","occ_Francini_temporal_abrolhos", "data_organization_benthos.R"))
-
-
-
-
-# organize data of Longo et al 2019
-
-source (here ("Data","occ_Longo_et_al", "Script_data_organization_Longo_et_al_2019.R"))
-
-
-
-
-# organize data of Mendes, Ferreira et al unpublished
-
-source (here ("Data","occ_Mendes_temporal_arraial", "data_organization.R"))
-
-
-
-# organize data of Morais et al. 2017
-
-source (here ("Data","occ_Morais_et_al", "organizacao_dados.R"))
-
-
-# fish and benthos at once
-
-source (here ("Data","occ_RN_Norte_Longo", "data_organization.R"))
-
-
-# PELD ILOC
-# a couple of adjustments in peld iloc datasets
-
-
-source (here ("Data","PELD_iloc_benthos", "data_organization.R"))
-source (here ("Data","PELD_iloc_fish", "data_organization.R"))
 
 
